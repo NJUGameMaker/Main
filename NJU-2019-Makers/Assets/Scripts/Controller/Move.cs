be59576f3c40630f67bb16c4cae1b5c764f8b0dc
LeoDarcy <baidurnt@163.com>
2019-09-28 17:32:06 +0800

    Update Move Function

--- NJU-2019-Makers/Assets/Scripts/Controller/Move.cs
+++ NJU-2019-Makers/Assets/Scripts/Controller/Move.cs
@@ -7,7 +7,7 @@ public class Move : MonoBehaviour
 {
 
 	//移动类型 
-	enum MoveType
+	public enum MoveType
 	{
 		Stop,
 		Line,
@@ -16,8 +16,18 @@ public class Move : MonoBehaviour
 		AIFollow
 		//TODO ...
 	};
-
-	//设置各种类型移动的参数并且设置各种初始化函数 TODO
+    //移动的类型
+    MoveType moveType = MoveType.Stop;
+    //中心
+    Vector2 heart;
+    //设置移动的类型
+    public void SetMoveType(MoveType mt, Vector2 position)
+    {
+        moveType = mt;
+        heart = position;
+    }
+	
+    //设置各种类型移动的参数并且设置各种初始化函数 TODO
 	public void Init()
 	{
 
@@ -26,7 +36,16 @@ public class Move : MonoBehaviour
 	//根据类型和参数进行移动 TODO
 	private void work()
 	{
-
+        //分开判断处理
+        switch (moveType)
+        {
+            case MoveType.Stop: { };break;
+            case MoveType.Line: { };break;
+            case MoveType.Round: { };break;
+            case MoveType.Cruve: { };break;
+            case MoveType.AIFollow: { };break;
+            default: Debug.LogAssertion("Wrong move type");break;
+        }
 	}
 
     // Update is called once per frame
