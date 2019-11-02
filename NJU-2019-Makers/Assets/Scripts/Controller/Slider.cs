c9b6fcb5dae507bd3d66d67595092f8ef0d18040
LeoDarcy <baidurnt@163.com>
2019-11-03 07:06:53 +0800

    bullet line

---
+++ NJU-2019-Makers/Assets/Scripts/Controller/Slider.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Slider : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    float currentAmount = 100;
+    float speed = -0.2f;
+    // Update is called once per frame
+    void Update()
+    {
+        currentAmount += speed;
+        if (currentAmount < 0)
+            currentAmount = 100;
+        GetComponent<Image>().fillAmount = currentAmount / 100.0f;
+        //GetComponent<Image>().color = new Color(1 - (currentAmount / 100.0f), (currentAmount / 100.0f), 0, (255 / 255f));
+    }
+    public void SetCurrentAmount(int bulletnum)
+    {
+        currentAmount = bulletnum;
+        
+    }
+}
