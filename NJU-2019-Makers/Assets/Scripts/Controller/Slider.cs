c4aaa76cd367771edaee782d53cddf7c1d4b12b9
karl07 <1474402804@qq.com>
2019-11-03 15:31:26 +0800

    laser

--- NJU-2019-Makers/Assets/Scripts/Controller/Slider.cs
+++ NJU-2019-Makers/Assets/Scripts/Controller/Slider.cs
@@ -11,20 +11,25 @@ public class Slider : MonoBehaviour
         
     }
     float currentAmount = 100;
-    float speed = -0.2f;
-    // Update is called once per frame
-    void Update()
+    const float speed = 0.3f;
+	// Update is called once per frame
+
+	private void FixedUpdate()
+	{
+		//currentAmount += speed;
+		//if (currentAmount < 0)
+		//currentAmount = 100;
+		SetCurrentAmount();
+		GetComponent<Image>().fillAmount = currentAmount / 100.0f;
+		//GetComponent<Image>().color = new Color(1 - (currentAmount / 100.0f), (currentAmount / 100.0f), 0, (255 / 255f));
+	}
+
+
+
+	public void SetCurrentAmount()
     {
-        //currentAmount += speed;
-        //if (currentAmount < 0)
-        //currentAmount = 100;
-        SetCurrentAmount();
-        GetComponent<Image>().fillAmount = currentAmount / 100.0f;
-        //GetComponent<Image>().color = new Color(1 - (currentAmount / 100.0f), (currentAmount / 100.0f), 0, (255 / 255f));
-    }
-    public void SetCurrentAmount()
-    {
-        currentAmount = PlayerManager.Instance.bullet / PlayerManager.Instance.maxBullet * 100;
+		var val = PlayerManager.Instance.bullet / PlayerManager.Instance.maxBullet * 100;
+		currentAmount += (val - currentAmount)*speed;
         
     }
 }
