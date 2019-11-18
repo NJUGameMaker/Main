2e08d38239462fa04f61300883a834e70d017abf
karl07 <1474402804@qq.com>
2019-11-18 19:09:14 +0800

    fix bugs

--- NJU-2019-Makers/Assets/Scripts/CallBack/CallBackMap1toMap2.cs
+++ NJU-2019-Makers/Assets/Scripts/CallBack/CallBackMap1toMap2.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class CallBackMap1toMap2 : CallBack
 {
+	public Map1Manager manager;
 	public override void Fun()
 	{
 		CameraManager.Instance.ReloadMap(3);
+		manager.save = Map1Manager.SavePoint.Map2Level1;
 	}
 }
