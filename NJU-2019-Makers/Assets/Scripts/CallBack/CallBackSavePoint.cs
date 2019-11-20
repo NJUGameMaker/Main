508e0a20cd7267b9a1af7b98042db94da82887f8
karl07 <1474402804@qq.com>
2019-11-20 17:05:24 +0800

    temp

--- NJU-2019-Makers/Assets/Scripts/CallBack/CallBackSavePoint.cs
+++ NJU-2019-Makers/Assets/Scripts/CallBack/CallBackSavePoint.cs
@@ -7,8 +7,10 @@ public class CallBackSavePoint : CallBack
 	public Map1Manager manager;
 	public Map1Manager.SavePoint save;
 	public int MapNum;
+	public bool End = false;
 	public override void Fun()
 	{
+		if (End) GameManager.Instance.StartGame("TempEnd");
 		CameraManager.Instance.ReloadMap(MapNum);
 		manager.ReLoad(manager.save = save);
 	}
